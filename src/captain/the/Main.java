2dab454ac18d40e2438e363c87c377b9e8f31fee
Captain <mads130f@edu.easj.dk>
2018-01-24 22:46:18 +0100

    Initial commit

---
+++ src/captain/the/Main.java
@@ -0,0 +1,19 @@
+package captain.the;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.settings.GameSettings;
+
+public class Main extends GameApplication {
+
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setWidth(800);
+        settings.setHeight(600);
+        settings.setTitle("Legends Arise");
+    }
+
+    public static void main(String args[]){
+        System.out.println("hello world!");
+        launch(args);   
+    }
+}
